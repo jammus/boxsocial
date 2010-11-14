0dc36244340bff7f1c66d3bafb8303c4ee52e67e
jammus <jammus@gmail.com>
2010-11-14 23:44:21 +0000

    Update against version 0.4.0 of lastfm-node

--- tests/party-test.js
+++ tests/party-test.js
@@ -11,10 +11,10 @@ var Gently = require('gently');
 
 ntest.describe("A new party");
     ntest.before(function() {
-        this.mockLastFm = new LastFmMocks.MockLastFm();
-        this.stream = new RecentTracksStream(this.mockLastFm, "hostuser");
+        this.lastfm = new LastFmNode();
+        this.stream = new RecentTracksStream(this.lastfm, "hostuser");
         this.stream.start = function() {}; // stub start to prevent tests hanging
-        this.party = new Party(this.stream);
+        this.party = new Party(this.lastfm, this.stream);
     });
 
     ntest.after(function() {
@@ -26,21 +26,21 @@ ntest.describe("A new party");
     });
 
     ntest.it("can add guests", function() {
-        var guest = new LastFmSession(this.mockLastFm, "guestuser1");
+        var guest = new LastFmSession(this.lastfm, "guestuser1");
         this.party.addGuest(guest);
         assert.equal(1, this.party.guests.length);
         assert.ok(this.party.guests.indexOf(guest) > -1);
     });
 
     ntest.it("can't add a guest twice", function() {
-        var guest = new LastFmSession(this.mockLastFm, "guestuser1");
+        var guest = new LastFmSession(this.lastfm, "guestuser1");
         this.party.addGuest(guest);
         this.party.addGuest(guest);
         assert.equal(1, this.party.guests.length);
     });
 
     ntest.it("can't add host to guest list", function() { 
-        var guest = new LastFmSession(this.mockLastFm, "hostuser");
+        var guest = new LastFmSession(this.lastfm, "hostuser");
         this.party.addGuest(guest);
         assert.equal(0, this.party.guests.length);
     });
@@ -49,22 +49,23 @@ ntest.describe("A new party");
         assert.ok(!this.stream.isStreaming);
         var gently = new Gently();
         gently.expect(this.stream, "start");
-        var guest = new LastFmSession(this.mockLastFm, "guestuser1");
+        var guest = new LastFmSession(this.lastfm, "guestuser1");
         this.party.addGuest(guest);
     });
 
 
 ntest.describe("A party in full swing");
     ntest.before(function() {
-        this.mockLastFm = new LastFmMocks.MockLastFm();
-        this.stream = new RecentTracksStream(this.mockLastFm, "hostuser");
+        this.lastfm = new LastFmNode();
+        this.stream = new RecentTracksStream(this.lastfm, "hostuser");
         this.stream.start = function() {}; // stub start to prevent tests hanging
-        this.party = new Party(this.stream);
-        this.guestOne = new LastFmMocks.MockLastFmSession(this.mockLastFm, "guestuser1", "authed1");
-        this.guestTwo = new LastFmMocks.MockLastFmSession(this.mockLastFm, "guestuser2", "authed2");
-        this.guestThree = new LastFmMocks.MockLastFmSession(this.mockLastFm, "guestthree" ,"authed3");
+        this.party = new Party(this.lastfm, this.stream);
+        this.guestOne = new LastFmSession(this.lastfm, "guestuser1", "authed1");
+        this.guestTwo = new LastFmSession(this.lastfm, "guestuser2", "authed2");
+        this.guestThree = new LastFmSession(this.lastfm, "guestthree" ,"authed3");
         this.party.addGuest(this.guestOne);
         this.party.addGuest(this.guestTwo);
+        this.gently = new Gently();
     });
 
     ntest.after(function() {
@@ -72,37 +73,54 @@ ntest.describe("A party in full swing");
     });
 
     ntest.it("shares now playing events with guests", function() {
+        this.gently.expect(this.lastfm, "update", function(method, session, options) {
+          assert.equal("nowplaying", method);
+          assert.equal("guestuser1", session.user);
+          assert.equal("Run To Your Grave", options.track.name);
+        });
+        this.gently.expect(this.lastfm, "update", function(method, session, options) {
+          assert.equal("nowplaying", method);
+          assert.equal("guestuser2", session.user);
+          assert.equal("Run To Your Grave", options.track.name);
+        });
         this.stream.emit('nowPlaying', FakeTracks.RunToYourGrave);
-        assert.equal(1, this.guestOne.nowPlayingCalls);
-        assert.equal("Run To Your Grave", this.guestOne.nowPlaying.name);
-        assert.equal(1, this.guestTwo.nowPlayingCalls);
-        assert.equal("Run To Your Grave", this.guestTwo.nowPlaying.name);
     });
 
     ntest.it("shares now playing with new guests", function() {
-        this.stream.emit('nowPlaying', FakeTracks.RunToYourGrave);
+        this.lastfm.update = function() {};
+        this.stream.emit("nowPlaying", FakeTracks.RunToYourGrave);
+        this.gently.expect(this.lastfm, "update", function(method, session, options) {
+          assert.equal("nowplaying", method);
+          assert.equal("guestthree", session.user);
+          assert.equal("Run To Your Grave", options.track.name);
+        });
         this.party.addGuest(this.guestThree);
-        assert.equal(1, this.guestThree.nowPlayingCalls);
     });
 
     ntest.it("shares scrobbles with guests", function() {
-        this.stream.emit('scrobbled', FakeTracks.RunToYourGrave);
-        assert.equal(1, this.guestOne.scrobbleCalls);
-        assert.equal("Run To Your Grave", this.guestOne.lastScrobbled.name);
-        assert.equal(1, this.guestTwo.scrobbleCalls);
-        assert.equal("Run To Your Grave", this.guestTwo.lastScrobbled.name);
+        this.gently.expect(this.lastfm, "update", function(method, session, options) {
+          assert.equal("scrobble", method);
+          assert.equal("guestuser1", session.user);
+          assert.equal("Run To Your Grave", options.track.name);
+        });
+        this.gently.expect(this.lastfm, "update", function(method, session, options) {
+          assert.equal("scrobble", method);
+          assert.equal("guestuser2", session.user);
+          assert.equal("Run To Your Grave", options.track.name);
+        });
+        this.stream.emit("scrobbled", FakeTracks.RunToYourGrave);
     });
 
     ntest.it("new guest doesnt receive now playing after stopped playing", function() {
-        this.stream.emit('nowPlaying', FakeTracks.RunToYourGrave);
-        this.stream.emit('stoppedPlaying', FakeTracks.RunToYourGrave);
+        this.gently.expect(this.lastfm, "update", 2, function() {});
+        this.stream.emit("nowPlaying", FakeTracks.RunToYourGrave);
+        this.stream.emit("stoppedPlaying", FakeTracks.RunToYourGrave);
         this.party.addGuest(this.guestThree);
-        assert.equal(0, this.guestThree.nowPlayingCalls);
         assert.equal(null, this.guestThree.nowPlaying);
     });
 
     ntest.it("returns false when checked for unknown guest", function() {
-        var guest = new LastFmSession(this.mockLastFm, "unknown", "huh");
+        var guest = new LastFmSession(this.lastfm, "unknown", "huh");
         assert.ok(!this.party.hasGuest(guest));     
     });
 
