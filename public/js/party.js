6fcf30934807892af41b5da306285107cfd31f7e
jammus <jammus@gmail.com>
2010-12-12 20:43:02 +0000

    Use ejs for client-side rendering too.

--- public/js/party.js
+++ public/js/party.js
@@ -1,40 +1,24 @@
 $(document).ready(function() {
     var host = $("div.host").data("host");
+    var guestsTpl = new EJS({ url:"/ejs/guests.ejs" });
+    var nowplayingTpl = new EJS({ url:"/ejs/nowplaying.ejs" });
 
     var handleGuestlist = function(guests) {
         if (guests.length == 0) {
             location.href = "/party/" + host;
+            return;
         }
-        else {
-            var list = $("#guestlist");
-            list.find("li").not(".header").remove();
-            for(var index in guests) {
-                var li = $("<li>");
-                var guest = guests[index];
-                if (guest.user && guest.user.image[2]["#text"]) {
-                    li.append($("<img>").attr("src", "http://getthumbsup.com/88b65e?url=" + guest.user.image[2]["#text"].replace("http://", "") + "&h=80&w=80"));
-                }
-                li.append($("<a>").attr("href", "http://www.last.fm/user/" + guest.session.user).text(guest.session.user));
-                list.append(li);
-            }
-            list.listview("refresh");
-        }
-
+        var html = guestsTpl.render({guests: guests});
+        var list = $("#guestlist");
+        list.find("li").not(".header").remove();
+        list.append(html);
+        list.listview("refresh");
     }
 
     var handleNowPlaying = function(track) {
-        var np = $("#nowPlaying");
-        np.empty();
-        if (track) {
-            if (track.image[2]["#text"]) {
-               np.append($("<img>").attr("src", track.image[2]["#text"])); 
-            }
-            np.append("Now playing: " + track.name + " by " + track.artist["#text"]);
-        }
-        else {
-            np.append("Now playing: Nothing");
-        }
-    }
+        var html = nowplayingTpl.render({party: {nowPlaying: track }});
+        $("#nowPlaying").html(html);
+    };
 
     var socket = new io.Socket();
     socket.on("connect", function() {
